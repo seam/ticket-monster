61058085a708ae3b6dd862681ff86426a05029a9
Pete Muir <pmuir@bleepbleep.org.uk>
2010-09-21 11:30:10 +0100

    whitespace

--- core/src/main/java/org/jboss/seam/example/ticketmonster/action/CacheConfig.java
+++ core/src/main/java/org/jboss/seam/example/ticketmonster/action/CacheConfig.java
@@ -7,16 +7,19 @@ import org.jboss.seam.example.ticketmonster.qualifier.RowCache;
 import org.jboss.seam.infinispan.Infinispan;
 
 /**
- * Configures various caches used throughout the app. 
+ * Configures various caches used throughout the app.
  * 
  * @author Shane Bryzak
- *
+ * 
  */
 public class CacheConfig
 {
-   public @Produces @Infinispan("rowCache") @RowCache Configuration getRowCacheConfiguration()
+   @Produces
+   @Infinispan("rowCache")
+   @RowCache
+   public Configuration getRowCacheConfiguration()
    {
       Configuration configuration = new Configuration();
       return configuration;
-   }   
+   }
 }
